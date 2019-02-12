49729ff39326ac847411296d38d124dec685490e
vagmi <sheladiavagmi@gmail.com>
2019-02-12 13:25:59 -0800

    index.js and node module added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("helloworld");
+const express = require('express');
+const app = express(); 
+const http = require('http');
+
+var data = {
+    id : 1,
+    name : 'vagmi'
+}
+
+app.get('/',(req,res)=>{
+    res.send(data);
+});
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
